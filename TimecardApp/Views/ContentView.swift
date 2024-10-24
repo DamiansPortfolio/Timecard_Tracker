b84f8d32f5f9605cda6f84b84eca8dbdd0451c1e
Anh Phan <aphan1@students.towson.edu>
2024-10-24 10:50:06 -0400

    Modified Tab Bar in ContentView

--- TimecardApp/Views/ContentView.swift
+++ TimecardApp/Views/ContentView.swift
@@ -4,19 +4,51 @@
 //
 //  Created by Damian Rozycki on 10/4/24.
 //
+
 import SwiftUI
 
 struct ContentView: View {
+    init() {
+        // Customize the appearance of the tab bar
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .white // Set the tab bar background to white
+        
+        // Customize tab bar item appearance
+        let itemAppearance = UITabBarItemAppearance()
+        let selectedColor = UIColor.systemTeal
+        let unselectedColor = UIColor.gray
+        
+        // Set colors for the selected state (teal)
+        itemAppearance.selected.titleTextAttributes = [.foregroundColor: selectedColor]
+        itemAppearance.selected.iconColor = selectedColor
+        
+        // Set colors for the unselected state (gray)
+        itemAppearance.normal.titleTextAttributes = [.foregroundColor: unselectedColor]
+        itemAppearance.normal.iconColor = unselectedColor
+        
+        appearance.stackedLayoutAppearance = itemAppearance
+        
+        UITabBar.appearance().standardAppearance = appearance
+        
+        // For iOS 15 and later, update the scrollEdgeAppearance as well
+        if #available(iOS 15.0, *) {
+            UITabBar.appearance().scrollEdgeAppearance = appearance
+        }
+    }
+
     var body: some View {
         TabView {
-            TimecardListView()
-                .tabItem {
-                    Label("Timecards", systemImage: "clock")
-                }
             JobCodeListView()
                 .tabItem {
                     Label("Job Codes", systemImage: "list.bullet")
                 }
+            
+            TimecardListView()
+                .tabItem {
+                    Label("Timecards", systemImage: "clock")
+                }
+            
             ProfileView()
                 .tabItem {
                     Label("Profile", systemImage: "person")
@@ -30,4 +62,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
