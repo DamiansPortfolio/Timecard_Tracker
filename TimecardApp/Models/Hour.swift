b9ca9cff45cf476c1b20f0bccb3239f47ce5ac59
Anastasia <arunion1@students.towson.edu>
2024-11-14 21:02:14 -0500

    no message

--- TimecardApp/Models/Hour.swift
+++ TimecardApp/Models/Hour.swift
@@ -5,7 +5,7 @@
 //  Created by Damian Rozycki on 11/14/24.
 //
 
-
+//comment for anastasia
 enum Hour: Int, CaseIterable, Identifiable {
     case h12am = 0, h1am, h2am, h3am, h4am, h5am, h6am, h7am, h8am, h9am, h10am, h11am
     case h12pm = 12, h1pm, h2pm, h3pm, h4pm, h5pm, h6pm, h7pm, h8pm, h9pm, h10pm, h11pm
